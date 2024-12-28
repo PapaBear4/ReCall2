51897dbe9fa0d156464ac2b8c452264cc3bcdb01
bear.sidell <bear.sidell@gmail.com>
2024-12-28 19:28:32 +0000

    fixes

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:ReCall2/contact/view/contact_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ReCall2/contact/contact.dart';
